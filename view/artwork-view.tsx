8ae3f296ac9dd7cb6f67e15e0c6b94fa48498344
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-19 21:29:31 +0300

    doc: view dir created

---
+++ view/artwork-view.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtworkView = () => {
+  return <div></div>;
+};
+
+export default ArtworkView;
---
+++ view/exhibition-view.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExhibitionView = () => {
+  return <div></div>;
+};
+
+export default ExhibitionView;
