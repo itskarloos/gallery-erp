694b5591df2501a61d31b3a9f0daa2f93f04f492
Michael <mikenegh@gmail.com>
2024-09-23 15:57:50 +0300

    Bug: Footer and Header fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/shared/footer";
-import Navbar from "@/components/shared/Navbar";
+import Navbar from "@/components/shared/navbar";
 
 export default function RootLayout({
   children,
