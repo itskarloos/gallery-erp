6a13deccea4d973f81cc1062a350673e9efc15da
Michael <mikenegh@gmail.com>
2024-09-23 15:30:08 +0300

    Page: Artwork page ✅

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface Artwork {
+  id: string;
+  artName: string;
+  artist: string;
+  date: number;
+  material: string;
+  dimensions: string;
+  imageUrl: string;
+}
--- view/home-view.tsx
+++ view/home-view.tsx
@@ -1,4 +1,4 @@
-import HeroSlideshow from "@/components/shared/HeroSlide";
+import HeroSlideshow from "@/components/shared/hero-slide";
 import React from "react";
 import HeroSlideshow from "@/components/shared/HeroSlide";
 
